f868b90a4305b7cb60bd11a4e99f80c6632c65f7
Dakoro <david.korobetski@gmail.com>
2023-03-01 13:24:05 +0100

    add test_forum.py

---
+++ test_forum.py
@@ -0,0 +1,53 @@
+import json, colorama
+from utils import factorielle, message_count
+def test_forum():
+    with open('datasets/sample.json') as f:
+        for line in f:
+            x = json.loads(line)
+            assert x != None
+            print('----------------------------------------------------')
+            n = message_count(x['content'])
+            assert n == x['content']['comments_count']+1
+
+def test_fact():
+    n = factorielle(1)
+    assert n == 1
+    n = factorielle(2)
+    assert n == 2
+    n = factorielle(3)
+    assert n == 6
+    n = factorielle(4)
+    assert n == 24
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
